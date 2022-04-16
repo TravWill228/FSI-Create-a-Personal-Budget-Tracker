fa4e66fbef0265d87e1aec279feaf66dd4c944e8
TravWill228 <travwilliams908@gmail.com>
2022-04-16 15:17:27 -0400

    First Personal Budget Push

--- index.js
+++ index.js
@@ -2,4 +2,56 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+
+}
+
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+
+}
+
+
+for(let i = 0; i < anualExpenseQuestions.length; i++){
+
+    let answer = parseFloat(window.prompt(anualExpenseQuestions[i]))
+    anualExpenses = monthlyExpenses + answer
+
+}
+
+
+
+
+
+
+
+
+// let weeklyExpenseQuestions = [
+//     `What's your weekly budget for lunches?`,
+//     `What's your weekly budget for entertainment?`,
+//     `What's your weekly budget for groceries?`
+// ]
+
+// let monthlyExpenseQuestions = [
+//     `What's your monthly rent budget?`,
+//     `What's your monthly insurance budget?`,
+//     `What's your monthly gas budget?`,
+//     'What are your monthly utility costs?'
+// ]
+
+// let annualExpenseQuestions = [
+//     `What's your annual subscription budget?`,
+//     `What's  your annual vacation budget?`,
+//     `What's your annual donation budget?`
+//  ]
+
+
+
+
+
